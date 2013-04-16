5f6171d85313420ad9781357fefaf6bd5cf8e444
Jelle Smeets <jsmeets91@gmail.com>
2013-04-16 11:15:01 +0200

    Project setup

---
+++ java_backend/src/java_backend/Java_backend.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_backend;
+
+/**
+ *
+ * @author Jelle
+ */
+public class Java_backend {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
