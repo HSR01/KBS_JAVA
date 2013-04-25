cdcb2172839d839de87a8199e9bf989aa2ca3fc0
Leon Huzen <leonhuzen@gmail.com>
2013-04-25 15:05:19 +0200

    Main, zonder ParseException

--- java_backend/src/java_backend/Java_backend.java
+++ java_backend/src/java_backend/Java_backend.java
@@ -1,10 +1,7 @@
 package java_backend;
 
-import org.json.simple.parser.ParseException;
-
 public class Java_backend {
-    public static void main(String[] args) throws ParseException {
-        // TODO code application logic here
+    public static void main(String[] args) {
         GUI g = new GUI();
     }
 }
