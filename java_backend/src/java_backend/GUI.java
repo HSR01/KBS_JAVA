b2b3ad1a3ecf30614c8d597225d23fdcc64f0ea1
Jelle Smeets <jsmeets91@gmail.com>
2013-04-25 13:54:54 +0200

    Cardlayout 1e opzet

--- java_backend/src/java_backend/GUI.java
+++ java_backend/src/java_backend/GUI.java
@@ -4,100 +4,92 @@
  */
 package java_backend;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextField;
+import javax.swing.*;
 
 /**
- *
+ * @author Jelle
  * @author Daniel
  */
 public class GUI extends JFrame implements ActionListener {
-
     private JLabel  logo;
     private JButton btNieuw, btOpslaan, btNorth;
-    private JPanel jInputfields, jNorth, jWest, jEast, jSouth, jFieldPanel;
+     private CardLayout cl = new CardLayout();
+    private JPanel cardHolder = new JPanel(cl);
+    //private JPanel jInputfields, jNorth, jWest, jEast, jSouth, jFieldPanel;
+   
     private JTable table;
     private String tableinhoud[] = {"ID", "Naam"};
     
-
     public GUI() {
+        //Standaard instellingen hoofdscherm
         super();
         this.setTitle("TZT Post");
-        this.setLayout(new BorderLayout());
+        this.setLayout(new FlowLayout());
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setResizable(false);
-        this.setSize(800, 600);
+        this.setSize(800, 600);        
         
-        DbConnect dbc = new DbConnect();
+        //try catch statement voor userdata in JTable te plaatsen.
         try{
+            DbConnect dbc = new DbConnect();
             dbc.getUsers();
             table = new JTable(dbc.getUsers(), tableinhoud);
         }
         catch(Exception e){
            System.out.println(e.getMessage());
         }
-        this.jInputfields = new JPanel();
+        
+        //Instellingen eerste pagina.
+        JPanel Card1 = new JPanel();
+        Card1.setLayout(new FlowLayout());
+        //Card1.add();
+        this.btOpslaan = new JButton("Opslaan");    
+        Card1.add(this.btOpslaan);
+        
+        //instellingen tweede pagina.
+        JPanel Card2 = new JPanel();
+        
+        Card2.add(table);
+        //Voeg kaarten toe aan de cardlayout.
+        cardHolder.add(Card1);
+        cardHolder.add(Card2);
+        this.add(cardHolder);
+        /*this.jInputfields = new JPanel();
         this.jNorth = new JPanel();
         this.jWest = new JPanel();
         this.jEast = new JPanel();
         this.jSouth = new JPanel();
         this.jFieldPanel = new JPanel();
-
         jFieldPanel.setLayout(new GridLayout(13, 2));
 
-       
-        
         this.logo = new JLabel();
         logo.setIcon(new javax.swing.ImageIcon("C:\\Users\\Daniel\\Google Drive\\KBS HSR\\tztklein.png"));
-
         this.btNieuw = new JButton("Nieuw");
-        this.btOpslaan = new JButton("Opslaan");
+           */
         
         btOpslaan.addActionListener(this);
-        btNieuw.addActionListener(this);
-        
-      
+        /*
+        btNieuw.addActionListener(this);    
         this.jWest.add(table);
-
         this.jInputfields.add(jFieldPanel);
-
         this.jSouth.add(btNieuw);
         this.jSouth.add(btOpslaan);
-
         this.jNorth.add(logo);
-        
-
         this.add(jNorth, BorderLayout.NORTH);
         this.add(new JScrollPane(table), BorderLayout.WEST);
         this.add(jInputfields, BorderLayout.EAST);
-
         this.add(jSouth, BorderLayout.SOUTH);
-
+*/
         this.setVisible(true);
-        
-
-        
     }
         public void actionPerformed(ActionEvent ae) {
-	if (ae.getSource() == btOpslaan) {
-             DbConnect a = new DbConnect();
-            
-             
-             
-	}            
-            
+            //button opslaan action performed
+            if (ae.getSource() == btOpslaan) {
+              this.cl.next(this.cardHolder);
+                
+            }                
         }
 }
