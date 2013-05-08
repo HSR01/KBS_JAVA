7854f58e7adc5f5f58a82685ef102f5ce923fed5
Daniel van der Berg <daniel.vander.berg@windesheim.nl>
2013-05-08 14:23:48 +0200

    Berekening koerier afgemaakt

--- java_backend/src/java_backend/Financien.java
+++ java_backend/src/java_backend/Financien.java
@@ -1,5 +1,7 @@
 package java_backend;
 
+import java.sql.Array;
+
 /**
  *
  * @author Jelle
@@ -10,23 +12,54 @@ public class Financien {
         
     }
     /**
-     * @author Jelle Smeets
-     * @param km
-     * @return 
+     * @author Daniel van der Berg (bitch!)
      */
-    public Koerier BerekenKoerier(int km){
+    
+    //Theo Snel en zonen gegevens
+    private final int TheoSnelenZonen = 1;
+    private double TS_Perkm = 0.20;
+    private double TS_Starttarief = 5.00;
+    private int TS_Startm = 25000;
+    //------------------------->
+    
+    //Sjors fiets koeriers gegevens
+    private final int SjorsFietsKoeriers = 2;
+    private double SF_Perkm = 0.25;
+    private double SF_Starttarief = 0.00;
+    private int SF_Startm = 0;
+    //------------------------->
+    
+    //Jansen transport diensten
+    private final int JansenTransport = 3;
+    private double JT_Perkm = 0.00;
+    private double JT_Starttarief = 20.00;
+    private int JT_Startm = 400000;
+    //------------------------->  
+    
+    public double[] BerekenKoerier(int m){   
+        
+        double ret = 0;
+        double type = 0;
+       
+        double TS = m > TS_Startm ? TS_Perkm * ((m - TS_Startm) / 1000) + TS_Starttarief : TS_Starttarief;        
+        double SF = m > SF_Startm ? SF_Perkm * ((m - SF_Startm) / 1000) + SF_Starttarief : SF_Starttarief;
+        double JT = m > JT_Startm ? JT_Perkm * ((m - JT_Startm) / 1000) + JT_Starttarief : JT_Starttarief;
+        
+        if(TS <= SF && TS <= JT){
+            ret = TS;
+            type = TheoSnelenZonen;
+        }if(SF <= TS && SF <= JT){
+            ret = SF;
+            type = SjorsFietsKoeriers;
+        }if(JT <= TS && JT <= SF){
+            ret = JT;  
+            type = JansenTransport;
+        }
+        
+        double[] result = {ret,type};
         
         // nog even koeriers objecten vanuit de database inladen om te returnen.
-        if(km <= 19){
-            //fietskoerier
-            System.out.println("fietskoerier");
-        }else if(km  == 20){
-            //fietskoerier of theo en zonen (random)
-            System.out.println("Random Theo en zonen of Fietskoerier");
-        }else if (km >20){
-            //theo en zonen
-            System.out.println("Theo en zonen");
-        }
-        return null;
+        return result;
+ 
     }
 }
