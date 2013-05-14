76833b738a7b92529e62f5ff3cc60eb0b4628c90
Dominique te Riele <dominiqueteriele@hotmail.com>
2013-05-14 21:59:37 +0200

    Update2 traject toewijzen
    niet af maar moet syncen om bij justins edits te komen :P

--- java_backend/src/java_backend/BPSTrajectToewijzen.java
+++ java_backend/src/java_backend/BPSTrajectToewijzen.java
@@ -19,9 +19,109 @@ import javax.swing.event.ListSelectionListener;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableModel;
 
-   
 public class BPSTrajectToewijzen extends JFrame implements ActionListener, ListSelectionListener {
 
+    private JTable aTable;
+    private String geselecteerdeWaarde;
+    
+    public BPSTrajectToewijzen(){     
+        TrajectTabel();
+        TrajectInstellingen();
+    }
+    
+    public void TrajectInstellingen(){
+        this.setSize(800,600);
+        this.setVisible(true);
+    }
+    
+    public void TrajectTabel(){
+        final String[] tabelinhoud = {"ID", "Voornaam", "Tussenvoegsel", "Achternaam", "Emailadres", "Wachtwoord", "Geboortedatum", "Mobiel", "Foto", "IBAN", "Rechten"};
+        DbConnect dbc = new DbConnect();
+        final Object[][] data = dbc.getUsers();
+        
+        TableModel dataModel = new AbstractTableModel() {
+            @Override
+            public int getColumnCount() { return tabelinhoud.length; }
+            @Override
+            public int getRowCount() { return data.length;}
+            @Override
+            public Object getValueAt(int row, int col) { return data[row][col]; }
+            @Override
+            public String getColumnName(int column) {return tabelinhoud[column];}
+            //@Override
+            //public Class getColumnClass(int col) {
+              //  return getValueAt(0,col).getClass();
+            //}
+            @Override
+            public void setValueAt(Object aValue, int row, int column) {
+                data[row][column] = aValue;
+            }
+        };
+
+        aTable = new JTable(dataModel);
+        aTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        aTable.getColumnModel().getColumn(0).setPreferredWidth(25);
+        aTable.getColumnModel().getColumn(1).setPreferredWidth(70);
+        aTable.getColumnModel().getColumn(2).setPreferredWidth(40);
+        aTable.getColumnModel().getColumn(3).setPreferredWidth(70);
+        aTable.getColumnModel().getColumn(4).setPreferredWidth(150);
+        aTable.getColumnModel().getColumn(6).setPreferredWidth(70);
+        aTable.getColumnModel().getColumn(7).setPreferredWidth(70);
+        aTable.getColumnModel().getColumn(8).setPreferredWidth(70);
+        aTable.getColumnModel().getColumn(9).setPreferredWidth(40);
+        aTable.getColumnModel().getColumn(10).setPreferredWidth(25);
+        
+        
+
+        this.add(new JScrollPane(aTable));
+      
+        
+        ListSelectionModel listMod = aTable.getSelectionModel();
+        listMod.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        listMod.addListSelectionListener( this );
+        
+         
+        aTable.addMouseListener(
+            new MouseAdapter(){
+
+                @Override
+                public void mouseClicked(MouseEvent e){
+                    if (e.getClickCount() == 2){
+                        DbConnect dbc = new DbConnect();
+                        final String[] specifiekeGebruikerGegevens = dbc.getSpecifiekeGebruikerGegevens(geselecteerdeWaarde);
+                        System.out.println(specifiekeGebruikerGegevens);
+                        final String[] specifiekeGebruikerLocatie = dbc.getSpecifiekeGebruikerLocatie(geselecteerdeWaarde);
+                        WijzigPersoon wijzigData = new WijzigPersoon(specifiekeGebruikerGegevens, specifiekeGebruikerLocatie);
+                    }
+                }
+            }
+        );
+        
+        this.add(aTable);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void valueChanged(ListSelectionEvent e) {
+        TableModel tm = aTable.getModel();
+        int[] selRows = aTable.getSelectedRows();
+        Object geselecteerdeWaarde = tm.getValueAt(selRows[0],0);
+        getSelecteerdeWaarde(geselecteerdeWaarde);
+    }
+        
+    public void getSelecteerdeWaarde(Object string){
+        this.geselecteerdeWaarde = (String) string;
+    }
+}
+
+/*
+public class BPSTrajectToewijzen extends JFrame implements ActionListener, ListSelectionListener {
+    
+    
     // Hoofdframe
     private JPanel jpButtons, jpTable, jpHoofd, jpZoek, jpZoekLabels, jpZoekInvulvelden;
     private JButton jbVervers, jbZoekBPS, jbZoekTraject;
@@ -200,3 +300,4 @@ public class BPSTrajectToewijzen extends JFrame implements ActionListener, ListS
     }
     
 }
+*/
