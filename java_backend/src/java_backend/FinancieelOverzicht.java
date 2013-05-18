c9f97c7a0f1103d91b9d2e5c2e93b084694bb3a2
Jelle Smeets <jsmeets91@gmail.com>
2013-05-18 13:15:50 +0200

    jtable toegevoegd financieel overzicht

--- java_backend/src/java_backend/FinancieelOverzicht.java
+++ java_backend/src/java_backend/FinancieelOverzicht.java
@@ -4,16 +4,17 @@
  */
 package java_backend;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableModel;
 
 /**
  *
@@ -23,15 +24,21 @@ class FinancieelOverzicht extends JPanel implements ActionListener{
     private JComboBox selectie;
     private JLabel jaar;
     private Calendar now;
-    
+    private JTable info;
             
     public FinancieelOverzicht() {
         //instancieer velden;
-        now = Calendar.getInstance();
-        //maak combobox voor jaren selecteren.
+        this.now = Calendar.getInstance();
+        
 
+        //instancieer JTable met datamodel van hierboven.
+        this.info = new JTable(dataModel);
+        
+        //maak combobox voor jaren selecteren.
         DefaultComboBoxModel jaren = new DefaultComboBoxModel();
         jaren.addElement("Kies een jaar.");
+        
+        //voeg jaren toe aan de hand van het huidige jaar na 2013.
         if (this.now.get(Calendar.YEAR) > 2013) {
             for (int h = 2013; h < (this.now.get(Calendar.YEAR) + 1); h++) {
                 jaren.addElement(h);
@@ -39,16 +46,26 @@ class FinancieelOverzicht extends JPanel implements ActionListener{
         } else {
             jaren.addElement("2013");
         }
-        //instancieer combobox
+        //instancieer combobox met gevulde jaren erin.
         this.selectie = new JComboBox(jaren);
-                
-        //voeg layout to aan jpanel
-        this.add(new JLabel("Kies een jaar"));
-        this.add(this.selectie);
+        
+        //set de layout van het panel op borderlayout
+        this.setLayout(new BorderLayout());
+        //voeg layout to aan jpanel top
+        JPanel top =new JPanel();
+        top.add(new JLabel("Kies een jaar"));
+        top.add(this.selectie);
+        //voeg layout toe aan Jpanel mid
+        JPanel mid = new JPanel();
+        mid.add(this.info);
+        //voeg uiteindelijk alles toe aan het centrale geheel.
+        this.add(top, BorderLayout.NORTH);
+        this.add(mid, BorderLayout.CENTER);
         
         //add actionlisteners
         this.selectie.addActionListener(this);
         
+        //tonen maar
         this.setVisible(true);
     }
 
@@ -62,8 +79,22 @@ class FinancieelOverzicht extends JPanel implements ActionListener{
                 //een waarde heeft een jaar. Ga door voor query om de boel op te halen en alles.
                 DbConnect dbc = new DbConnect();
                 
+                
+                
             }
         }
     }
+    /**
+     * @author Jelle
+     * @description Gebruikt om de JTabel mee te vullen.
+     */
+    TableModel dataModel = new AbstractTableModel() {
+          //geef aantal kolomen terug
+          public int getColumnCount() { return 10; }
+          //geef aantal rijen terug.
+          public int getRowCount() { return 10;}
+          //geef waarde terug om rij mee te vullen
+            public Object getValueAt(int row, int col) { return new Integer(row*col); }
+        };
     
 }
