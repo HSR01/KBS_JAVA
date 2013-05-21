0555017f4f4542bb36b2342a94baeeb5b87a93c0
Dominique te Riele <dominiqueteriele@hotmail.com>
2013-05-21 13:53:19 +0200

    Update om te kunne syncen

--- java_backend/src/java_backend/PersoonTraject.java
+++ java_backend/src/java_backend/PersoonTraject.java
@@ -1,9 +1,13 @@
 package java_backend;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
@@ -16,14 +20,18 @@ import javax.swing.table.TableModel;
  *
  * @author Justin
  */
-public class PersoonTraject extends JFrame implements ListSelectionListener {
+public class PersoonTraject extends JFrame implements ActionListener, ListSelectionListener {
  private int geselecteerdeWaarde;
  private String[] geselecteerdeWaardes;
  public static  JTable aTable;  
-    
+ private JButton jbNieuw, jbSluit;   
     public PersoonTraject(final int geselecteerdeWaarde) {
         super();
         this.geselecteerdeWaarde = geselecteerdeWaarde;
+        jbNieuw = new JButton("Nieuw");
+        jbSluit = new JButton("Sluit");
+        jbNieuw.addActionListener( this );
+        jbSluit.addActionListener( this );
         
         this.setSize(800, 600);
         //this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -57,7 +65,12 @@ public class PersoonTraject extends JFrame implements ListSelectionListener {
         aTable.getColumnModel().getColumn(1).setPreferredWidth(70);
         aTable.getColumnModel().getColumn(2).setPreferredWidth(40);     
         
+        this.setLayout(new BorderLayout());
         this.add(new JScrollPane(aTable));
+        JPanel knoppen = new JPanel();
+        knoppen.add( jbNieuw );
+        knoppen.add( jbSluit );
+        this.add ( knoppen, BorderLayout.SOUTH);
       
         ListSelectionModel listModel = aTable.getSelectionModel();
         listModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -84,7 +97,12 @@ public class PersoonTraject extends JFrame implements ListSelectionListener {
     }
     
     public void actionPerformed(ActionEvent ae) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if( ae.getSource() == jbNieuw){
+            BPSTrajectToewijzen test = new BPSTrajectToewijzen(geselecteerdeWaarde);
+        }
+        if( ae.getSource() == jbSluit){
+            this.setVisible(false);
+        }
     }
      
     public void valueChanged(ListSelectionEvent e) {
