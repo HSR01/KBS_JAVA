021073abf183cd000578a1895749dcc4fc486348
Jelle Smeets <jsmeets91@gmail.com>
2013-05-21 20:39:35 +0200

    Feedback af!

--- java_backend/src/java_backend/Feedback.java
+++ java_backend/src/java_backend/Feedback.java
@@ -9,21 +9,25 @@ package java_backend;
  * @author Laurens
  */
 public class Feedback {
-    private int Waardering;
+    private int Waardering, FeedbackID, Ontvangststatus, PakketID;
     private String Omschrijving;
-    private int Ontvangststatus;
+
     
     public Feedback() {
         this.Waardering = 0;
         this.Omschrijving = "";
         this.Ontvangststatus = 0;
+        this.FeedbackID = 0;
+        this.PakketID = 0;
         
     }
     
-    public Feedback(int w, String om, int o) {
+    public Feedback(int w, String om, int o, int f, int p) {
         Waardering = w;
         Omschrijving = om;
         Ontvangststatus = o;
+        this.FeedbackID = f;
+        this.PakketID = p;
     }
 
     /**
@@ -45,6 +49,55 @@ public class Feedback {
      */
     public int getOntvangststatus() {
         return Ontvangststatus;
+    }
+
+    /**
+     * @param Waardering the Waardering to set
+     */
+    public void setWaardering(int Waardering) {
+        this.Waardering = Waardering;
+    }
+
+    /**
+     * @return the FeedbackID
+     */
+    public int getFeedbackID() {
+        return FeedbackID;
+    }
+
+    /**
+     * @param FeedbackID the FeedbackID to set
+     */
+    public void setFeedbackID(int FeedbackID) {
+        this.FeedbackID = FeedbackID;
+    }
+
+    /**
+     * @param Ontvangststatus the Ontvangststatus to set
+     */
+    public void setOntvangststatus(int Ontvangststatus) {
+        this.Ontvangststatus = Ontvangststatus;
+    }
+
+    /**
+     * @return the PakketID
+     */
+    public int getPakketID() {
+        return PakketID;
+    }
+
+    /**
+     * @param PakketID the PakketID to set
+     */
+    public void setPakketID(int PakketID) {
+        this.PakketID = PakketID;
+    }
+
+    /**
+     * @param Omschrijving the Omschrijving to set
+     */
+    public void setOmschrijving(String Omschrijving) {
+        this.Omschrijving = Omschrijving;
     }
     
     
--- java_backend/src/java_backend/FeedbackPanel.java
+++ java_backend/src/java_backend/FeedbackPanel.java
@@ -5,9 +5,12 @@
 package java_backend;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.ScrollPane;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
@@ -16,6 +19,9 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableModel;
 
@@ -23,7 +29,7 @@ import javax.swing.table.TableModel;
  *
  * @author Jelle
  */
-class FeedbackPanel extends JPanel implements ActionListener{
+class FeedbackPanel extends JPanel implements ActionListener, ListSelectionListener{
     private JPanel top, mid;
     private JLabel toplabel, zoeklabel;
     private JTextField zoekveld;
@@ -64,15 +70,71 @@ class FeedbackPanel extends JPanel implements ActionListener{
 
         //voeg action listeners toe
         this.zoek.addActionListener(this);
+        
+        //handel click event af.
+        ListSelectionModel listMod = this.info.getSelectionModel();
+        //zet selectie modus op enkele regel
+        listMod.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        // Voegt de listener toe aan het frame
+        listMod.addListSelectionListener(this);
+        
+        //voeg mouse listener toe op jtable
+        this.info.addMouseListener(
+            new MouseAdapter() {
+                @Override
+                // Mouseclick event.
+                public void mouseClicked(MouseEvent e) {
+                    // Zodra je 2x achter elkaar snel klikt word deze code uitgevoerd. 
+                    //dubbelklik
+                    if (e.getClickCount() == 2) {
+                        //haal de geselecteerde rij op.
+                        int row = info.rowAtPoint(e.getPoint());
+                        //haal pakketid op vanuit formulier
+                        int pakid = Integer.parseInt(zoekveld.getText());
+                        
+                        
+                        //haal feedback object op aan de hand van row id.
+                        //lelijke oplossing op het moment
+                        DbConnect dbc = new DbConnect();
+                        Feedback[] list = dbc.getFeedback(pakid);
+                        Feedback spec = list[row];
+                        
+                        //maak een jdialog pop up box om alle info in te tonen.
+                        
+                        JDialog feedbackinfo = new JDialog();
+                        feedbackinfo.setSize(300, 300);
+                        feedbackinfo.setLayout(new GridLayout(6,2));
+                        feedbackinfo.add(new JLabel("FeedbackID"));
+                        feedbackinfo.add(new JLabel(""+spec.getFeedbackID()));
+                        feedbackinfo.add(new JLabel("PakketID"));
+                        feedbackinfo.add(new JLabel(""+spec.getPakketID()));
+                        feedbackinfo.add(new JLabel("Waardering"));
+                        feedbackinfo.add(new JLabel("" + spec.getWaardering() + " " + "Sterren"));
+                        feedbackinfo.add(new JLabel("Omschrijving"));
+                        feedbackinfo.add(new JLabel(spec.getOmschrijving()));
+                        feedbackinfo.add(new JLabel("Ontvangststatus"));
+                        feedbackinfo.add(new JLabel(""+spec.getOntvangststatus()));
+                        feedbackinfo.setVisible(true);
+
+                        
+                        
+                    }
+                }
+            }
+        );
+        
     }
-    
+        @Override
+    public void valueChanged(ListSelectionEvent e) {
+
+    }
     public TableModel FillTable(final int PakketID){
          TableModel dataModel = new AbstractTableModel() {
          //set colum names.
-         final String[] tabelinhoud = {"FeedbackID", "PakketID", "Waardering", "Omschrijving", "Ontvangststatus"};
+         final String[] tabelinhoud = {"FeedbackID", "PakketID", "Waardering"};
          //instancier database connectie en haal alle feedback op.
          private DbConnect dbc = new DbConnect();
-         Object[][] results = dbc.getFeedback(PakketID);
+         Feedback[] results = dbc.getFeedback(PakketID);
          
          /**
           * @author Jelle
@@ -96,7 +158,7 @@ class FeedbackPanel extends JPanel implements ActionListener{
              */
             @Override
             public int getColumnCount() {
-                return 5;
+                return 3;
                 //throw new UnsupportedOperationException("Not supported yet.");
             }
             /*
@@ -108,7 +170,14 @@ class FeedbackPanel extends JPanel implements ActionListener{
                 if(results == null){
                     return 0;
                 }else{
-                    return results[row][col];
+                    //haal per kolom dmv getters de juiste waarde op om in het formulier te tonen.
+                    if(col == 0){
+                        return results[row].getFeedbackID();
+                    }else if (col == 1){
+                        return results[row].getPakketID();
+                    }else{
+                        return results[row].getWaardering();
+                    }
                 }
                 
             }
